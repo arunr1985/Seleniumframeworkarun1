0fb88f97efb1e6c85d11d709373123920c8e82db
arunr1985 <arunr@192.168.2.7>
2020-06-23 00:42:10 +0530

    arun3 committed

---
+++ eclipse-workspace/arun3again/src/arun3again/arun3againclass.java
@@ -0,0 +1,12 @@
+package arun3again;
+
+public class arun3againclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Yahoo");
+
+	}
+
+}
