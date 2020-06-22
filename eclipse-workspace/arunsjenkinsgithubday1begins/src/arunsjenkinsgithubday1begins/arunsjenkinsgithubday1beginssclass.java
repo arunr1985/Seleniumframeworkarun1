47f2a7083cb9b1feda48aec93a86853062f3beca
arunr1985 <arunr@192.168.2.7>
2020-06-22 19:06:00 +0530

    Commit this as urgent

---
+++ eclipse-workspace/arunsjenkinsgithubday1begins/src/arunsjenkinsgithubday1begins/arunsjenkinsgithubday1beginssclass.java
@@ -0,0 +1,16 @@
+package arunsjenkinsgithubday1begins;
+
+import org.testng.annotations.Test;
+
+public class arunsjenkinsgithubday1beginssclass 
+
+{
+
+	@Test
+	void getdata()
+	{
+		System.out.println("Yahoo");
+	}
+	
+	
+}
