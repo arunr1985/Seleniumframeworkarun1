48f60f08b0c8f9c5e97445795d34b70741b5309a
arunr1985 <arunr@192.168.2.7>
2020-06-22 18:35:42 +0530

    THis is my new proj

---
+++ eclipse-workspace/arunsjenkinsgithubday1begins/src/arunsjenkinsgithubday1begins/arunsjenkinsgithubday1beginss.java
@@ -0,0 +1,14 @@
+package arunsjenkinsgithubday1begins;
+
+import org.testng.annotations.Test;
+
+public class arunsjenkinsgithubday1beginss 
+{
+
+	@Test
+	void getthisprinted()
+	{
+		System.out.println("Never Ever GiveUp!!");
+	}
+	
+}
